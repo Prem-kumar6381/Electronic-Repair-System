9e828bdc498581d550fa03e51483d64f26b47fcb
prem kumar <149067613+Prem-kumar6381@users.noreply.github.com>
2024-08-03 10:31:20 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+function para1(){
+    document.getElementById("paragraph1").innerHTML="This website explains smartphones, laptops, tablets, video games, and how to repair these electronic items."
+}
+function para2(){
+    document.getElementById("paragraph1").style.color="blue"
+}
+function nav1(){
+    alert("home was clicked")
+}
+function nav2(){
+    alert("Certification was clicked")
+}
+function nav3(){
+    alert("Contacts was clicked")
+}
+function nav4(){
+    alert("check status was clicked")
+}
